51d55e0c50a74269aa999783a40b97b3f304523d
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-06-16 16:28:57 +0800

    sm8550-common: add AOSP deskclock
    For PowerOff Alarm
    
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -107,6 +107,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# DeskClock
+PRODUCT_PACKAGES += \
+    DeskClock
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
