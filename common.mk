5185e88cc925ec354eef6f39b1617a07d572b207
weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2025-09-05 03:11:41 +0800

    sm8550-common: ship init script for OPlusExtras
    Signed-off-by: weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -194,6 +194,7 @@ PRODUCT_PACKAGES += \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
+    init.oplus_extras.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
