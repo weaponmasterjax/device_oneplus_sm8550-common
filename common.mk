6fea666d4e65a730b0bebceaaae1782f5283e8e7
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-07-11 20:38:32 +0800

    Revert "sm8550-common: build JamesDSP and its manager"
    This reverts commit f5283d9a45759da4984fa489992f612b852a995d.
    
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -448,6 +448,3 @@ PRODUCT_PACKAGES += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8550-common/sm8550-common-vendor.mk)
-
-# Inherit from JamesDSPManager 
-$(call inherit-product, packages/apps/JamesDSPManager/config.mk)
