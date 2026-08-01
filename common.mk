3141e0bd531081ea2c3b8daa58f64d09a76eda40
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-08-02 03:25:07 +0800

    sm8550-common: Update Dalvik heap size configuration to 12GiB
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-12288-dalvik-heap.mk)
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
